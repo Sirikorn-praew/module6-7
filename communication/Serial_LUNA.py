1ba29bd4fb5396da8bc49a88ab982e981b327e8d
Sirikorn-praew <56629248+Sirikorn-praew@users.noreply.github.com>
2020-12-06 14:35:55 +0700

    LUNA_Ui

--- communication/Serial_LUNA.py
+++ communication/Serial_LUNA.py
@@ -1,20 +1,23 @@
 import serial
 import time
-serialPic = serial.Serial(port= 'COM5',timeout = 3, baudrate=115200,
+serialPic = serial.Serial(port= 'COM3',timeout = 3, baudrate=115200,
                   xonxoff=0, rtscts=0,bytesize=8, parity='N', stopbits=1)
 serialPic.rts = 0
 serialPic.dtr = 0
 serialPic.timeout = 0.1
 serialPic.flush()
+listdata = [[100,100,100,0,1],[]]
 
-def toByte(x,y,z,a):
+def toByte(x,y,z,a,g):
     din = [int(x),int(y),int(z),int(a)]
     bytelist = []
     for i in din:
         bytelist.append(i//256)
         bytelist.append(i%256)
+    bytelist.append(int(g))
     return bytelist
 # Data FF F_ xx xx yy yy zz zz aa aa gg
+
 def Sethome():
     data = [255,241,0,0,0,0,0,0,0,0,0]                            #"FF,F1" + x + y + z + a + g
     #print(data)
@@ -37,21 +40,30 @@ def Sethome():
 def Capture():
     data = [255,242,0,0,0,0,0,0,0,0,0]                            #"FF,F2"
     serialPic.write(serial.to_bytes(data))
+    print(data)
     state = 1
     while(True):
         a = serialPic.readline()
-        print(a)
-        spic = [b for b in a][0]
-        if spic == state:
-            print("state = "+ str(state))
-            serialPic.write(serial.to_bytes(spic))
-            state += 1
-        if spic == 99:
-            print("error");
-            serialPic.write(serial.to_bytes(data))
-            state = 1
-        if spic == 242:
-            break
+        if len(a) != 0:
+            print(a)
+            spic = [b for b in a][0]
+            ac = [255,170,spic,0,0,0,0,0,0,0,0]
+            if spic == 5:
+                print("state = "+ str(state))
+                time.sleep(5)
+                serialPic.write(serial.to_bytes(ac))
+                print(ac)
+                break
+            elif spic == state:
+                print("state = "+ str(state))
+                time.sleep(5)
+                serialPic.write(serial.to_bytes(ac))
+                print(ac)
+                state += 1
+            if spic == 99:
+                print("error");
+                serialPic.write(serial.to_bytes(data))
+                state = 1
     print("Capture Finish")
 
 def Catch():
@@ -67,7 +79,7 @@ def Catch():
                 break
     print("Catch!")
 
-def Move(m):
+def MoveI(m):
     while True:
         x = input("X-axis : ")
         if x == 'q':
@@ -85,13 +97,16 @@ def Move(m):
         if a == 'q':
             print('quit')
             break
-        byteL = toByte(x,y,z,a)
-        byteL.append(0)
+        g = input("Gripper : ")
+        if g == 'q':
+            print('quit')
+            break
+        byteL = toByte(x,y,z,a,g)
         if m == "PID":
             data = [255,244,byteL[0],byteL[1],byteL[2],byteL[3],byteL[4],byteL[5],byteL[6],byteL[7],byteL[8]]    #"FF,F4" + x + y + z
         if m == "Traj":
             data = [255,245,byteL[0],byteL[1],byteL[2],byteL[3],byteL[4],byteL[5],byteL[6],byteL[7],byteL[8]]    #"FF,F5" + x + y + z
-        #print(data)
+        print(data)
         serialPic.write(serial.to_bytes(data))
         while(True):
             a = serialPic.readline()
@@ -106,6 +121,28 @@ def Move(m):
                         break    
         print("Move to x="+ x +" , y="+ y + " , z="+ z)
         break
+
+def Move(m,list):
+    for i in list:    
+        byteL = toByte(list[0],list[1],list[2],list[3],list[4])
+        if m == "PID":
+            data = [255,244,byteL[0],byteL[1],byteL[2],byteL[3],byteL[4],byteL[5],byteL[6],byteL[7],byteL[8]]    #"FF,F4" + x + y + z
+        if m == "Traj":
+            data = [255,245,byteL[0],byteL[1],byteL[2],byteL[3],byteL[4],byteL[5],byteL[6],byteL[7],byteL[8]]    #"FF,F5" + x + y + z
+        print(data)
+        serialPic.write(serial.to_bytes(data))
+        while(True):
+            a = serialPic.readline()
+            if len(a) != 0:
+                print(a)
+                spic = [b for b in a][0]
+                if m == "PID":
+                    if spic == 244:
+                        break
+                if m == "Traj":
+                    if spic == 245:
+                        break    
+        print("Move to x="+ x +" , y="+ y + " , z="+ z)
 
 def Reset():
     data = [255,187,0,0,0,0,0,0,0,0,0]                            #"FF,BB" 
